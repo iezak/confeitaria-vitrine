8917f2919bf714f456adf75c7d768ad452edd8fd
iezak <iezak53@gmail.com>
2026-05-12 23:29:02 -0300

    feat: configura rotas principais da aplicação (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Catalogo from "./pages/Catalogo";
+import Sobre from "./pages/Sobre";
+import Contato from "./pages/Contato";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div>
-      <h1>Confeitaria Vitrine</h1>
-      <p>Projeto inicial em React</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/contato" element={<Contato />} />
+    </Routes>
   );
 }
 
