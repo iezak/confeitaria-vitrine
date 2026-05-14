bfd8760c8918a5fd235267a8d328ea3393992b85
iezak <iezak53@gmail.com>
2026-05-13 23:58:00 -0300

    feat: cria arquivo de dados iniciais de categorias e produtos

---
+++ src/data/categorias.js
@@ -0,0 +1,10 @@
+export const categorias = [
+  { id: 1, nome: "Caseirinhos" },
+  { id: 2, nome: "Bolos com frutas" },
+  { id: 3, nome: "Bolos premium" },
+  { id: 4, nome: "Tortas" },
+  { id: 5, nome: "Brigadeiros tradicionais" },
+  { id: 6, nome: "Brigadeiros gourmet" },
+  { id: 7, nome: "Doces especiais" },
+  { id: 8, nome: "Salgados" },
+];
